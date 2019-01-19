f595d50a0997ab3ca980e7180fb7c9362e2f7ed8
Jishaal Kalyan <jishaal@gmail.com>
2019-01-20 11:05:36 +1300

    initial commit

---
+++ src/pages/posts/2018-09-16-what-is-function.length/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `What is function.length?`,
+  tags: ['javascript', 'development', 'learning to code'],
+  spoiler: "What does this property mean? Find out in this article, it may not be what you think.",
+  getContent: () => import('./document.mdx'),
+}
---
+++ src/siteMetadata.js
@@ -0,0 +1,11 @@
+export default {
+  // The blog's title as it appears in the layout header, and in the document
+  // <title> tag.
+  title: "jishaal.com",
+
+  author: "Jishaal Kalyan",
+  description: "The personal site of Jishaal Kalyan, a frontend developer from Auckland, New Zealand",
+
+  // The number of posts to a page on the site index.
+  indexPageSize: 10,
+}
